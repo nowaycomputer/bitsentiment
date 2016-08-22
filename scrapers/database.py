94d4d1956bd78abbfe69b7b8f406817a331ce667
chriswallace00 <chriswallace00@gmail.com>
2016-08-22 20:54:53 +0100

    added simple scrapers to get bitcoin price and twitter news stories every minute

--- scrapers/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class Mongo_Database: #
-    def __init__(self):
-        self.client=MongoClient()
-        self.db=self.client.test
-
-    def store_single(self, data):
-        result=self.db.bitsentiment.insert_one(data)   
--- scrapers/tester.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import requests
-import bitcoin_data_scrapers as bcs
-import database as db
-
-
-cb_scraper=bcs.Coinbase_Scraper()
-price_database=db.Mongo_Database()
-price_database.store_single(cb_scraper.get_spot_price())
